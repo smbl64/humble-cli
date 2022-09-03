93ff194be1239ee7c3b0eece596e2dd6fc740e5e
Mohammad Banisaeid <smbl64@gmail.com>
2022-09-03 08:48:16 +0200

    Improve the test case

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,5 +6,5 @@ fn runs() {
     let output = cmd.output().unwrap();
     assert_eq!(output.status.code(), Some(2));
     let msg = String::from_utf8(output.stderr).expect("failed to convert to String");
-    assert!(msg.contains("missing"));
+    assert!(msg.contains("The missing Humble Bundle CLI"));
 }
